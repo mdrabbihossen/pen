d906d744449f599a40d4f46874d82ca3df528020
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-07-24 20:26:04 +0600

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pen/views/home/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
+      home: const HomeScreen(),
     );
   }
 }
