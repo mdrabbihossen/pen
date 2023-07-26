2d4c119862d0d44d0ee0b924f4c061434292e898
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-07-26 20:19:53 +0600

    assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pen/views/auth/login/screens/login_screen.dart';
 import 'package:pen/views/home/screens/home_screen.dart';
 
 void main() async {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
